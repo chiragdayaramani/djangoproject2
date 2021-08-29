04b4247115d4f47d3324e36ebefa9e1e89af6a5b
CHIRAG <chiragddayaramani@gmail.com>
2021-08-29 11:35:59 +0530

    posts and category models

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,8 @@
+from posts.models import Category
 from django.contrib import admin
+from posts.models import Category,Post
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Category)
+
