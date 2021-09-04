caf156e799d051ea91c9c128bc99aad1c13fe76c
CHIRAG <chiragddayaramani@gmail.com>
2021-09-04 11:21:21 +0530

    created create.html template

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,10 @@
+from collections import namedtuple
 from django.urls import path
 from . import views as post_views
 
 #127.0.0.1:8000/posts/
 urlpatterns=[
     path('<str:slug>/',post_views.post,name='post'),
-    path('',post_views.index,name='posts')
+    path('',post_views.index,name='posts'),
+    path('create',post_views.create,name='create')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,30 @@
+from posts.forms import PostForm
 from django.shortcuts import render, HttpResponse
 from .models import Category, Post
 # Create your views here.
-def post(request, slug): 
-    post = Post.objects.filter(slug = slug).first()
+
+
+def post(request, slug):
+    post = Post.objects.filter(slug=slug).first()
     return HttpResponse(f"<h1> {post.title} </h1> <br> <p> {post.content}</p>")
+
 
 def index(request):
     post = Post.objects.all()
     print(post)
     return HttpResponse("Done")
 
+
+def create(request):
+
+    if request.method == 'POST':
+
+        form = PostForm(request.POST)
+        post = form.save()
+        return HttpResponse(post.title)
+    else:
+        form = PostForm()
+        context = {
+            'form': form,
+        }
+        return render(request, 'create.html', context)
