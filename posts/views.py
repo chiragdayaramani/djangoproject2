4e94c6c1700dc47172e96b5765f5fa3e8bb2a5d1
CHIRAG <chiragddayaramani@gmail.com>
2021-09-05 11:22:53 +0530

    created post.html

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,17 @@
 from posts.forms import PostForm,CategoryForm
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render, HttpResponse
 from .models import Category, Post
 # Create your views here.
 
 
 def post(request, slug):
-    post = Post.objects.filter(slug=slug).first()
-    return HttpResponse(f"<h1> {post.title} </h1> <br> <p> {post.content}</p>")
+    # post = Post.objects.filter(slug=slug).first()
+    # return HttpResponse(f"<h1> {post.title} </h1> <br> <p> {post.content}</p>")
+    post=get_object_or_404(Post,slug=slug)
+    context={
+        'post':post
+    }
+    return render(request,'post.html',context)
 
 
 def index(request):
@@ -18,9 +23,12 @@ def index(request):
 def create(request):
 
     if request.method == 'POST':
+
         form = PostForm(request.POST)
+        form.instance.author=request.user
         post = form.save()
-        return HttpResponse(post.title)
+        return redirect("post",slug=post.slug)
+        # return HttpResponse(post.title)
     else:
         form = PostForm()
         context = {
